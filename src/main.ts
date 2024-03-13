9e3740106e009d46afd1d7f27d9467055e1d28f7
kimhans <fogy431@gmail.com>
2024-03-13 20:09:30 +0900

    ➕ swagger - 추가

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ResponseTransformInterceptor } from './todo/interceptors/response-transform-interceptor';
 import { LoggingInterceptor } from './todo/interceptors/logging-interceptor';
+import { ResponseTransformInterceptor } from './todo/interceptors/response-transform-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /** Swagger API 문서화 설정  */
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LoggingInterceptor());
